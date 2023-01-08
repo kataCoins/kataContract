f9e965f64a88127d1d7941e2bb6853c17856b061
Paul Abib <paul_abib@connect-tech.sncf>
2023-01-08 20:51:37 +0100

    rectification on the hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-web3";
-import "./tasks/faucet.js";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
